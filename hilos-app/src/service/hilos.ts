0287d7f088cfc3f528bfe3e7796714c0fb60f2c0
juanjo-0210 <juan.jose.mateo.0210@gmail.com>
2024-05-10 10:40:27 +0200

    subida

---
+++ hilos-app/src/service/hilos.ts
@@ -0,0 +1,60 @@
+import { db, collection, addDoc, getDocs, query, doc, deleteDoc } from "./api";
+import { getDoc, updateDoc, type QuerySnapshot } from "firebase/firestore";
+
+
+const collectionName:string = 'hilos';
+
+// CREATE
+export const createPersons = async(obj) => {
+    const colRef = collection(db, collectionName);
+    const data = await addDoc(colRef, obj);
+    return data.id;
+}
+
+    //CON TYPESCRIPT
+    export const createPersonsTypeS = async (obj) => {
+    const colRef = collection(db,collectionName);
+    const data = await addDoc(colRef, obj);
+    return data.id;
+    };
+
+
+
+
+// READ
+export const getItems = async ()  => {
+    const colRef = collection(db, collectionName);
+    const result = await getDocs(query(colRef));
+    return getArrayFromCollection(result);
+}
+
+    //CON TYPESCRIPT
+    export const getTasks = async (arg:string):Promise<string> => {
+    const colRef = collection(db, arg);
+    const result = await getDocs(query(colRef));
+    return getArrayFromCollection(result);
+    };
+
+    export const getItemById = async (id:string):Promise<string> => {
+    const docRef = doc(db, collectionName, id);
+    const result = await getDoc(docRef);
+    return result.data();
+}
+
+// UPDATE
+export const updateItem = async (id, obj) => {
+    const docRef = doc(db, collectionName, id);
+    await updateDoc(docRef, obj)
+}
+
+// DELETE
+export const deleteItem = async (id:string) => {
+    const docRef = doc(db, collectionName, id);
+    await deleteDoc(docRef);
+}
+
+const getArrayFromCollection = (collection: QuerySnapshot):DocData[] => {
+    return collection.docs.map(doc => {
+        return { ...doc.data(), id: doc.id };
+    });
+}
